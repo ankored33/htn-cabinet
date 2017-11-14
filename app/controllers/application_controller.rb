62c1c5c0d14599d6a0144943170cf29aaaa0c64f
ankored33 <ankored33@gmail.com>
2017-11-15 00:56:04 +0900

    show ministers list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  MINISTERS = ["内閣総理大臣", "総務大臣", "法務大臣", "外務大臣", "財務大臣", "文部科学大臣", "厚生労働大臣", "農林水産大臣", "経済産業大臣", "国土交通大臣", "環境大臣", "防衛大臣", "内閣官房長官", "国家公安委員会委員長", "内閣府特命担当大臣"]
+  POSITIONS = {
+    sori: "内閣総理大臣", 
+    somu: "総務大臣", 
+    homu: "法務大臣", 
+    gaimu: "外務大臣",
+    zaimu: "財務大臣",
+    monbukagaku: "文部科学大臣",
+    koseirodo: "厚生労働大臣",
+    norinsuisan: "農林水産大臣",
+    keizaisangyo: "経済産業大臣",
+    kokudokotsu: "国土交通大臣", 
+    kankyo: "環境大臣", 
+    boei: "防衛大臣", 
+    kanbo: "内閣官房長官", 
+    koan: "国家公安委員会委員長", 
+    tokumei_masuda: "増田担当大臣"
+    }
 end
--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -1,6 +1,12 @@
 class CabinetsController < ApplicationController
   def show
-    @date = params[:date]
-    @ministers = MINISTERS
+    @date = Date.today
+    positions = POSITIONS
+    cabinet = Cabinet.find_by(id: "1") #テスト用
+    #cabinet = Cabinet.find_by(date: @date)
+    @cabinet = {}
+    positions.each do |k, v|
+      @cabinet[v] = cabinet.send(k)
+    end
   end
 end
